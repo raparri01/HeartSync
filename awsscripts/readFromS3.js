3fda6420e2c05a190ef94d84fcd74136b821b40c
ROBERT APARRI <robertaparri@MacBook-Pro-4.local>
2018-02-20 16:29:50 -0500

    alteration to s3upload and readme

--- awsscripts/readFromS3.js
+++ awsscripts/readFromS3.js
@@ -11,24 +11,18 @@ AWS.config.update({
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 // call S3 to retrieve upload file to specified bucket
-var uploadParams = {Bucket: "heartsync-s3-bucket/Users/3lkqtfou9xllnul3qsy07ks2cbqiho1w", Key: '', Body: ''};
-var file = process.argv[2];
+var parameters = {Bucket: "heartsync-s3-bucket/Users/3lkqtfou9xllnul3qsy07ks2cbqiho1w/currentStatus.json", Key: '', Body: ''};
+var file = "banddataexample.json";
 
 var fs = require('fs');
 var fileStream = fs.createReadStream(file);
 fileStream.on('error', function(err) {
   console.log('File Error', err);
 });
-uploadParams.Body = fileStream;
+parameters.Body = fileStream;
 
 var path = require('path');
-uploadParams.Key = path.basename(file);
+parameters.Key = path.basename(file);
 
 // call S3 to retrieve upload file to specified bucket
-s3.upload (uploadParams, function (err, data) {
-  if (err) {
-    console.log("Error", err);
-  } if (data) {
-    console.log("Upload Success", data.Location);
-  }
-});
+s3.getObject
